4c7315a017fa4dce264b4e98713107adbe674f1e
icarlos1 <ruizdominguezcarlos@icloud.com>
2019-03-14 16:20:03 -0600

    finale

---
+++ frontend/src/components/Tepoztlan.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+
+export default function(){
+   
+   
+    return(
+
+      
+      
+      <div> 
+
+      <img className="Tepoz" src="https://www.visitmexico.com/viajemospormexico/assets/uploads/destinos/morelos_destinos-principales_tepoztlan_01.jpg"/>
+    
+      </div>
+
+
+    )
+}
